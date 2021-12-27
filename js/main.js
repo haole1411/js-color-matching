51ec6b5ae3a3a84c9e67aeec5beff956e51018c6
haole1411 <18130070@st.hcmuaf.edu.vn>
2021-12-27 17:11:00 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
-import { GAME_STATUS } from './constants'
+import { GAME_STATUS, GAME_TIME, PAIRS_COUNT } from './constants.js'
+import {
+  getColorElementList,
+  getColorListElement,
+  getInActiveColorList,
+  getPlayAgainButton,
+} from './selectors.js'
+import { createTimer, getRandomColorPairs, hideReplay, setTimerText, showReplay } from './utils.js'
 
 // Global variables
 let selections = []
-let gameState = GAME_STATUS.PLAYING
+let gameStatus = GAME_STATUS.PLAYING
+let timer = createTimer({
+  second: GAME_TIME,
+  onChange: handleTimerChange,
+  onFinish: handleTimerFinish,
+})
+function handleTimerChange(second) {
+  const fullSecond = `0${second}`.slice(-2)
+  setTimerText(fullSecond)
+}
+function handleTimerFinish() {
+  //end game
+  gameStatus = GAME_STATUS.FINISHED
+  setTimerText('Game Over')
+}
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
@@ -10,3 +31,119 @@ let gameState = GAME_STATUS.PLAYING
 // 3. Check win logic
 // 4. Add timer
 // 5. Handle replay click
+console.log(getRandomColorPairs(PAIRS_COUNT))
+
+function initColor() {
+  //random 8 pair of color
+  const colorList = getRandomColorPairs(PAIRS_COUNT)
+  //bind to li > div.overlay
+  const liList = getColorElementList()
+  liList.forEach((liElement, index) => {
+    liElement.dataset.color = colorList[index]
+    const overlayElement = liElement.querySelector('.overlay')
+    if (overlayElement) overlayElement.style.backgroundColor = colorList[index]
+  })
+}
+function attachEventForColorList() {
+  const ulElement = getColorListElement()
+  if (!ulElement) return
+  ulElement.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'LI') return
+    handleColorList(event.target)
+  })
+}
+
+//handleColorCLick 1
+//handleColorCLick 2
+//handleColorCLick 3
+//setTImeout 2 --> reset selec
+//setTImeout 3 --> error here
+
+function handleColorList(liElement) {
+  const shouldBlockCLick = [GAME_STATUS.BLOCKING, GAME_STATUS.FINISHED].includes(gameStatus)
+  const isCLick = liElement.classList.contains('active')
+  if (!liElement || isCLick || shouldBlockCLick) return
+  liElement.classList.add('active')
+  // save clicked cell to selection
+  selections.push(liElement)
+
+  if (selections.length < 2) {
+    return
+  }
+
+  //check math
+  const firstColor = selections[0].dataset.color
+  const secondColor = selections[1].dataset.color
+
+  const isMatch = firstColor === secondColor
+  if (isMatch) {
+    //check win
+    const isWin = getInActiveColorList().length === 0
+    if (isWin) {
+      //show replay
+      showReplay()
+      //show you win
+      setTimerText('You Win !!!!')
+      timer.clear()
+
+      gameStatus = GAME_STATUS.FINISHED
+    }
+
+    selections = []
+    return
+  }
+  //in case of not match
+  //remove active for 2 li element
+  gameStatus = GAME_STATUS.BLOCKING
+  setTimeout(() => {
+    selections[0].classList.remove('active')
+    selections[1].classList.remove('active')
+    selections = []
+    //race conditino check with handleTImeeFinish
+    if (gameStatus !== GAME_STATUS.FINISHED) {
+      gameStatus = GAME_STATUS.PLAYING
+    }
+  }, 500)
+  //reset
+}
+
+function resetGame() {
+  //reset  global var
+  gameStatus = GAME_STATUS.PLAYING
+  selections = []
+  // reset DOM element
+  //remove active class form li
+  // hide replay btt
+  // clear you win text
+  const colorElementList = getColorElementList()
+  for (const colorElement of colorElementList) {
+    colorElement.classList.remove('active')
+  }
+  hideReplay()
+  setTimerText('')
+
+  // re-generate colorList
+  initColor()
+
+  //start new Game
+  startTimer()
+}
+
+function attachEventReplayButton() {
+  const playAgainButton = getPlayAgainButton()
+  if (!playAgainButton) return
+  playAgainButton.addEventListener('click', resetGame)
+}
+
+function startTimer() {
+  timer.start()
+}
+;(() => {
+  initColor()
+
+  attachEventForColorList()
+
+  attachEventReplayButton()
+
+  startTimer()
+})()
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,71 @@
+import { getPlayAgainButton, getTimerElement } from './selectors.js'
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+  const colorList = []
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome']
+
+  //random 'count' colors
+  for (let i = 0; i < count; i++) {
+    // randomColor function is provide by https://github.com/davidmerfield/randomColor
+    const color = window.randomColor({
+      luminosity: 'dark',
+      hue: hueList[i % hueList.length],
+    })
+
+    colorList.push(color)
+  }
+
+  const fullColorList = [...colorList, ...colorList]
+  shuffle(fullColorList)
+
+  return fullColorList
+}
+function shuffle(arr) {
+  if (!Array.isArray(arr) || arr.length <= 2) return arr
+  for (let i = arr.length - 1; i > 1; i--) {
+    const j = Math.floor(Math.random() * i)
+
+    let tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+  }
+}
+export function showReplay() {
+  const repLayElement = getPlayAgainButton()
+  if (!repLayElement) return
+  repLayElement.classList.add('show')
+}
+export function hideReplay() {
+  const repLayElement = getPlayAgainButton()
+  if (!repLayElement) return
+  repLayElement.classList.remove('show')
+}
+export function setTimerText(text) {
+  const timerElement = getTimerElement()
+  if (!timerElement) return
+  timerElement.textContent = text
+}
+export function createTimer({ second, onChange, onFinish }) {
+  let interValid = null
+  function start() {
+    clear()
+    let currentSecond = second
+    interValid = setInterval(() => {
+      // if (onChange) onChange(currentSecond)
+      onChange?.(currentSecond)
+      currentSecond--
+      if (currentSecond < 0) {
+        clear()
+        onFinish?.()
+      }
+    }, 1000)
+  }
+  function clear() {
+    clearInterval(interValid)
+  }
+  return {
+    start,
+    clear,
+  }
 }
